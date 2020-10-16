c28cfb9da6d450d8c9818cadf6d933f189f7c138
Lee Howett <lee@meettally.com>
2020-10-16 14:48:37 -0700

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,15 @@ buildscript {
         google()
         mavenCentral()
     }
+
+    val kotlinVersion = "1.4.10"
+    val sqlDelightVersion: String by project
+
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
 }
 group = "com.jetbrains.handson"
