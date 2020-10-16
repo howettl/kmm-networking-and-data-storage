5ece201e311e141b4bdc511fea6b2a1a354b6fef
Lee Howett <lee@meettally.com>
2020-10-16 14:45:46 -0700

    Updated gradle version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,16 +14,16 @@ repositories {
 }
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.2.0")
+    implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
 }
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
     defaultConfig {
         applicationId = "com.jetbrains.handson.androidApp"
         minSdkVersion(24)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
     }
